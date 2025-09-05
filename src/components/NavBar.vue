6e4571841c3ce0d40a883dab0bebffb8edcf5152
mzk <mazeikadiogo@gmail.com>
2025-09-05 16:50:01 -0300

    ajustes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
+const emit = defineEmits<{
+  teste: [id: number];
+}>();
+// @click="$emit('teste', '/')"
+</script>
+
+<template>
+  <div class="flex py-4 px-2 mb-8 bg-white rounded-md shadow-2xl">
+    <RouterLink
+      class="mx-3 px-2 py-2 cursor-pointer rounded-md hover:bg-slate-700 hover:text-white active:p-1 active:mx-4 active:my-1 active:bg-slate-900 active:text-white"
+      to="/"
+    >
+      Início
+    </RouterLink>
+    <RouterLink
+      class="mx-3 px-2 py-2 cursor-pointer rounded-md hover:bg-slate-700 hover:text-white active:p-1 active:mx-4 active:my-1 active:bg-slate-900 active:text-white"
+      to="/lista"
+    >
+      Lista
+    </RouterLink>
+    <RouterLink
+      class="ml-auto mr-3 px-2 py-2 cursor-pointer rounded-md hover:bg-slate-700 hover:text-white active:p-1 active:mr-4 active:my-1 active:bg-slate-900 active:text-white"
+      to="/about"
+    >
+      Sobre
+    </RouterLink>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 export default {
-  purge: ['*'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
   plugins: [],
-}
+};
